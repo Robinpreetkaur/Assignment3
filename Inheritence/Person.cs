92a5a49624069ea6202d95b710fda3cb641a8b4e
Robin <rkaur750@my.centennialcollege.ca>
2020-06-19 23:10:45 -0400

    Add project files.

---
+++ Inheritence/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritence
+{
+    class Person
+    {
+        
+
+        // PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // CONSTRUCTOR(S)
+        public Person(string name, int age)
+        {
+            // property initialization
+            Name = name;
+            Age = age;
+        }
+
+        
+
+        // PUBLIC METHODS
+        public void SaysHello()
+        {
+            Console.WriteLine($"{Name} says Hello!");
+        }
+    }
+}
---
+++ Inheritence/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate object of type Person
+            Person robin = new Person("Robin", 20);
+
+            // use saysHello method with . accessor
+            robin.SaysHello();
+
+            Console.ReadLine();
+
+        }
+    }
+}
